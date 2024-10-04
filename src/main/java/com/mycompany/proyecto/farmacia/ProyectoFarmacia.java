0f3c67f2274e3c442934ef4b4a80cdeb4357cfe4
YeBoiAaron <60552917+YeBoiAaron@users.noreply.github.com>
2024-10-04 15:19:38 -0700

    Initial commit

---
+++ src/main/java/com/mycompany/proyecto/farmacia/ProyectoFarmacia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto.farmacia;
+
+/**
+ *
+ * @author Aaron
+ */
+public class ProyectoFarmacia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
