d8e02d82185fe770931c6c610c45d5298c5673b8
unknown <ang3lfco@outlook.com>
2024-11-30 19:16:15 -0700

    Modificacion del EmpleadoMapper para recibir Usuario

--- proyectoCapaNegocio/src/main/java/com/mappers/EmpleadoMapper.java
+++ proyectoCapaNegocio/src/main/java/com/mappers/EmpleadoMapper.java
@@ -15,10 +15,36 @@ import java.time.LocalDate;
 public class EmpleadoMapper {
     
     public static EmpleadoDTO toDTO(Empleado empleado) {
-        return new EmpleadoDTO(empleado.getNombreCompleto(), empleado.getNumeroTelefono(), empleado.getFechaNacimiento(), empleado.getTipoEmpleado());
+        EmpleadoDTO empleadoDTO = new EmpleadoDTO();
+        
+        // Asignar los atributos de Usuario (los que vienen de la clase Padre Usuario)
+        empleadoDTO.setNombreUsuario(empleado.getNombreUsuario());
+        empleadoDTO.setCorreo(empleado.getCorreo());
+        empleadoDTO.setContrasena(empleado.getContrasena());
+
+        // Asignar los atributos de Empleado (los que vienen de la clase hija Empleado)
+        empleadoDTO.setNombreCompleto(empleado.getNombreCompleto());
+        empleadoDTO.setNumeroTelefono(empleado.getNumeroTelefono());
+        empleadoDTO.setFechaNacimiento(empleado.getFechaNacimiento());
+        empleadoDTO.setTipoEmpleado(empleado.getTipoEmpleado());
+        
+        return empleadoDTO;
     }
     
-    public static Empleado toEntity(EmpleadoDTO empleadoDto) {
-        return new Empleado(empleadoDto.getNombreCompleto(), empleadoDto.getNumeroTelefono(), empleadoDto.getFechaNacimiento(), empleadoDto.getTipoEmpleado());
+    public static Empleado toEntity(EmpleadoDTO empleadoDTO) {
+        Empleado empleado = new Empleado();
+        
+        // Asignar los atributos de Usuario (los que vienen de la clase Padre UsuarioDTO)
+        empleado.setNombreUsuario(empleadoDTO.getNombreUsuario());
+        empleado.setCorreo(empleadoDTO.getCorreo());
+        empleado.setContrasena(empleadoDTO.getContrasena());
+
+        // Asignar los atributos de Empleado (los que vienen de la clase hija EmpleadoDTO)
+        empleado.setNombreCompleto(empleadoDTO.getNombreCompleto());
+        empleado.setNumeroTelefono(empleadoDTO.getNumeroTelefono());
+        empleado.setFechaNacimiento(empleadoDTO.getFechaNacimiento());
+        empleado.setTipoEmpleado(empleadoDTO.getTipoEmpleado());
+        
+        return empleado;
     }
 }
